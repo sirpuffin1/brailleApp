f8759848647b40caeabcb77737d260e48aa760b9
sirpuffin1 <angelcruz981@yahoo.com>
2021-10-28 15:49:26 -0700

    good spot

--- src/app/components/button-display/button-display.component.ts
+++ src/app/components/button-display/button-display.component.ts
@@ -9,7 +9,7 @@ export class ButtonDisplayComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
+  toggleClass: boolean = false;
   toggleClass1: boolean = false;
   toggleClass2: boolean = false;
   toggleClass3: boolean = false;
@@ -32,26 +32,74 @@ export class ButtonDisplayComponent implements OnInit {
     '134': 8,
     '23': 9,
   };
-  combinationString = ''
+
+  letterValues: any = {
+    '1': 'A',
+    '13': 'B',
+    '12': 'C',
+    '124': 'D',
+    '14': 'E',
+    '123': 'F',
+    '1234': 'G',
+    '134': 'H',
+    '23': 'I',
+    '234': 'J',
+    '15': 'K',
+    '135': 'L',
+    '125': 'M',
+    '1245': 'N',
+    '145': 'O',
+    '1235': 'P',
+    '12345': 'Q',
+    '1345': 'R',
+    '235': 'S',
+    '2345': 'T',
+    '156': 'U',
+    '1356': 'V',
+    '2346': 'W',
+    '1256': 'X',
+    '12456': 'Y',
+    '1456': 'Z',
+  };
+  combinationString = '';
+  resultString = '';
+  translationType = 'Numbers';
 
   pushToArray(string: string) {
-
     this.combinationArray.push(string);
   }
 
   displayResult() {
-    console.log(this.combinationArray);
-    this.combinationArray.sort()
-    this.combinationString = this.combinationArray.join('')
-    console.log(this.combinationString)
-    const resultValue = this.numberValues[this.combinationString];
-    console.log(resultValue)
-    this.resultArray.push(resultValue)
-    console.log(this.resultArray)
+    this.combinationArray.sort();
+    this.combinationString = this.combinationArray.join('');
+    if (this.translationType == 'Numbers') {
+      const resultValue = this.numberValues[this.combinationString];
+      this.resultArray.push(resultValue);
+      this.resultString = this.resultArray.join('');
+    } else {
+      const resultValue = this.letterValues[this.combinationString];
+      this.resultArray.push(resultValue);
+      this.resultString = this.resultArray.join('');
+    }
+
     this.resetResult();
   }
 
   resetResult() {
     this.combinationArray = [];
+    this.toggleClass1 = false;
+    this.toggleClass2 = false;
+    this.toggleClass3 = false;
+    this.toggleClass4 = false;
+    this.toggleClass5 = false;
+    this.toggleClass6 = false;
+  }
+
+  switchTranslationType() {
+    if (this.translationType == 'Numbers') {
+      this.translationType = 'Letters';
+    } else {
+      this.translationType = 'Numbers';
+    }
   }
 }
