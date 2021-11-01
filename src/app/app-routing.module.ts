1926ad2822b1fcfbd747767b09e925a180af30e8
sirpuffin1 <angelcruz981@yahoo.com>
2021-11-01 15:40:15 -0700

    fine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonDisplayComponent } from './components/button-display/button-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'button-display', component: ButtonDisplayComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
